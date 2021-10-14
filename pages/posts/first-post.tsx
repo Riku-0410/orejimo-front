c22e960b6a6aa8d74b90a00a3249881f131b7621
Riku-0410 <srikuya.0410@icloud.com>
2021-10-15 00:14:57 +0900

    お勉強

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout home>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1 className = "text-9xl">First Post</h1>
       <h2 className = "bg-red-500 p-6">
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
